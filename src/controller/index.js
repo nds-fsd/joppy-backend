019532e9161c2aeb11e1d7cc88bd5d2b6a7e0ffc
laurasosa93 <78755131+laurasosa93@users.noreply.github.com>
2021-04-04 17:41:17 +0200

    Update index.js

--- src/controller/index.js
+++ src/controller/index.js
@@ -1,11 +1,9 @@
-const UserController = require('./userController');
 const RoleController = require('./roleController');
 const CityController = require('./cityController');
 
 
 
 module.exports = {
-  UserController,
   RoleController,
   CityController
 };

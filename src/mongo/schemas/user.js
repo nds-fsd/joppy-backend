d4dd904a4e9171b138181220b9ee3c8feab5ca31
Laura <laura.lsa93@gmail.com>
2021-04-12 01:18:18 +0200

    company populada, intento con user falla

--- src/mongo/schemas/user.js
+++ src/mongo/schemas/user.js
@@ -1,39 +1,54 @@
-const mongoose = require("mongoose");
+/** @format */
+
+const mongoose = require('mongoose');
 
 const schema = new mongoose.Schema(
-  {
-    userName: { type: String, required: true },
-    email: { type: String, required: true },
-    password: {
-      type: String,
-      required: true,
-      match: /(?=.*[a-zA-Z])(?=.*[0-9]+).*/,
-      minlength: 8,
-    },
-    bio: { type: String, required: true },
-    roles: {
-      type: mongoose.Schema.Types.ObjectId,
-      required: true,
-      ref: "Roles",
-    },
-    skills: { type: String, required: true },
-    workExperiences: { type: String, required: false },
-    education: { type: String, required: true },
-    languages: { type: String, required: true },
-    salary: {
-      currency: { type: String },
-      range: {
-        min: { type: Number, min: 0 },
-        max: { type: Number, min: 0 },
-      },
-    },
-    companySize: { type: Number, required: false },
-    typeEmployement: { type: String, required: true },
-    city: { type: mongoose.Schema.Types.ObjectId, required: true, ref: "City" },
-  },
-  { timestamps: { createdAt: "createdAt", updatedAt: "updatedAt" } }
+	{
+		userName: { type: String, required: true },
+		email: { type: String, required: true },
+		password: {
+			type: String,
+			required: true,
+			match: /(?=.*[a-zA-Z])(?=.*[0-9]+).*/,
+			minlength: 8,
+		},
+		bio: { type: String, required: true },
+		photo: { type: String, required: true },
+		roles: [
+			{
+				type: mongoose.Schema.Types.ObjectId,
+				required: true,
+				ref: 'Roles',
+			},
+		],
+		skills: [
+			{
+				type: mongoose.Schema.Types.ObjectId,
+				required: true,
+				ref: 'Skills',
+			},
+		],
+		workExperiences: [{ type: String, required: false }],
+		education: { type: String, required: false },
+		languages: [{ type: String, required: true }],
+		salary: {
+			currency: { type: String },
+			range: {
+				min: { type: Number, min: 0 },
+				max: { type: Number, min: 0 },
+			},
+		},
+		companySize: { type: Number, required: false },
+		typeEmployement: { type: String, required: false },
+		city: {
+			type: mongoose.Schema.Types.ObjectId,
+			required: true,
+			ref: 'City',
+		},
+	},
+	{ timestamps: { createdAt: 'createdAt', updatedAt: 'updatedAt' } }
 );
 
-const User = mongoose.model("User", schema);
+const User = mongoose.model('User', schema);
 
 module.exports = User;

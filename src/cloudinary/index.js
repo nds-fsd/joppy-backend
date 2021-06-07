fb061ff23542a160195db40cd1364e2cc4fe7d6c
Laura <laura.lsa93@gmail.com>
2021-06-07 22:25:50 +0200

    cloudinary ok

--- src/cloudinary/index.js
+++ src/cloudinary/index.js
@@ -8,9 +8,6 @@ cloudinary.config({
   api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
-// app.use(express.json({ limit: "50mb" }));
-// app.use(express.urlencoded({ limit: "50mb", extended: true }));
-
 const ImageRouter = new express.Router();
 
 ImageRouter.post("/upload", async (req, res) => {
@@ -18,7 +15,7 @@ ImageRouter.post("/upload", async (req, res) => {
   const id = req.params.id;
   try {
     const uploadedResponse = await cloudinary.uploader.upload(data, { upload_preset: "wchygywm" });
-    res.status(200).json(uploadedResponse.url);
+    res.status(200).json(uploadedResponse);
     console.log(uploadedResponse.url);
   } catch (error) {
     res.status(500).json(error);
